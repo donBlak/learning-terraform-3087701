4c998603fa19304407d7dac01f51b7941d256d74
donBlak <57069893+donBlak@users.noreply.github.com>
2024-02-29 20:40:04 +0530

    test 3

--- main.tf
+++ main.tf
@@ -27,21 +27,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog-autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "7.4.0"
-  
-  name     = "blog-as"
-  min_size = 1
-  max_size = 2
-  
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
 
-}
 
 data "aws_vpc" "default" {
   default = true
